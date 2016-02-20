ce65378dbe38a685b95f4e91adbf8364658ea44a
Hannes Hergeth <hannes.hergeth@rwth-aachen.de>
2016-02-20 03:12:22 +0100

    Added 32bit memset

--- Base/Platform.cu
+++ Base/Platform.cu
@@ -1,6 +1,7 @@
 #include <StdAfx.h>
 #include "Platform.h"
 #include <string.h>
+#include <cstdint>
 
 #ifdef ISWINDOWS
 #include <Windows.h>
@@ -56,6 +57,44 @@ void Platform::SetMemory(void* dest, size_t length, unsigned char val)
 {
 	if (dest && length)
 		memset(dest, val, length);
+}
+
+void Platform::SetMemoryExt(void* p, size_t n, unsigned int c)
+{
+	if (!p || !n)
+		return;
+
+	//code from here : http://www.xs-labs.com/en/blog/2013/08/06/optimising-memset/
+
+	uint8_t  * sp;
+	uint64_t * lp;
+	uint64_t   u64;
+	uint8_t    u8;
+
+	u8 = (uint8_t)c;
+	u64 = (uint64_t)c;
+	u64 = (u64 << 32) | u64;
+	sp = (uint8_t *)p;
+
+	while (n-- && (((uint64_t)sp & (uint64_t)-8) < (uint64_t)sp))
+	{
+		*(sp++) = u8;
+	}
+
+	lp = (uint64_t *)((void *)sp);
+
+	while ((n / 8) > 0)
+	{
+		*(lp++) = u64;
+		n -= 8;
+	}
+
+	sp = (uint8_t *)((void *)lp);
+
+	while (n--)
+	{
+		*(sp++) = u8;
+	}
 }
 
 void Platform::OutputDebug(const std::string& msg)
