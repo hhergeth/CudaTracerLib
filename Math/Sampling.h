62871e3b449dc0755bbcd57ea2e57d4dcfa4541a
Hannes Hergeth <hannes.hergeth@rwth-aachen.de>
2016-07-20 22:56:11 +0200

    Added variance calculation helper

--- Math/Sampling.h
+++ Math/Sampling.h
@@ -286,4 +286,70 @@ public:
 	}
 };
 
+template<typename T> struct VarAccumulator
+{
+	T Sum_X;
+	T Sum_X2;
+
+	CUDA_FUNC_IN VarAccumulator()
+		: Sum_X(0), Sum_X2(0)
+	{
+		
+	}
+
+	CUDA_FUNC_IN VarAccumulator(const T& sum, const T& sum2)
+		: Sum_X(sum), Sum_X2(sum2)
+	{
+
+	}
+
+	CUDA_FUNC_IN VarAccumulator& operator+=(const T& rhs)
+	{
+		Sum_X += rhs;
+		Sum_X2 += math::sqr(rhs);
+		return *this;
+	}
+
+	CUDA_FUNC_IN void Add(const T& X, const T& X2)
+	{
+		Sum_X += X;
+		Sum_X2 += X2;
+	}
+
+	CUDA_FUNC_IN T E(float sampleSize) const
+	{
+		return Sum_X / sampleSize;
+	}
+
+	CUDA_FUNC_IN T Var(float sampleSize) const
+	{
+		return (Sum_X2 - math::sqr(Sum_X) / sampleSize) / sampleSize;
+	}
+
+	CUDA_FUNC_IN VarAccumulator operator+(const VarAccumulator& rhs) const
+	{
+		return VarAccumulator(Sum_X + rhs.Sum_X, Sum_X2 + rhs.Sum_X2);
+	}
+
+	CUDA_FUNC_IN VarAccumulator operator-(const VarAccumulator& rhs) const
+	{
+		return VarAccumulator(Sum_X - rhs.Sum_X, Sum_X2 - rhs.Sum_X2);
+	}
+
+	CUDA_FUNC_IN VarAccumulator operator*(const VarAccumulator& rhs) const
+	{
+		return VarAccumulator(Sum_X * rhs.Sum_X, Sum_X2 * rhs.Sum_X2);
+	}
+
+	CUDA_FUNC_IN VarAccumulator operator*(float rhs) const
+	{
+		return VarAccumulator(Sum_X * rhs, Sum_X2 * rhs * rhs);
+	}
+
+	CUDA_FUNC_IN VarAccumulator operator/(float rhs) const
+	{
+		return this->operator*(1.0f / rhs);
+	}
+};
+
 }
