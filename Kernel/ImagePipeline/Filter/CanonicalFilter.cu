603eb3a9f192041ed02b34bc179b018339966535
Hannes Hergeth <hannes.hergeth@rwth-aachen.de>
2016-11-25 03:14:52 +0100

    fixed warnings, simplified code and removed -0.5f pixel move

--- Kernel/ImagePipeline/Filter/CanonicalFilter.cu
+++ Kernel/ImagePipeline/Filter/CanonicalFilter.cu
@@ -3,18 +3,12 @@
 namespace CudaTracerLib
 {
 
-CUDA_FUNC_IN Spectrum evalFilter(const Filter& filter, PixelData* P, float splatScale, unsigned int _x, unsigned int _y, unsigned int w, unsigned int h)
+CUDA_FUNC_IN Spectrum evalFilter(const Filter& filter, PixelData* P, float splatScale, int _x, int _y, int w, int h)
 {
-	float dimageX = _x - 0.5f;
-	float dimageY = _y - 0.5f;
-	int x0 = math::Ceil2Int(dimageX - filter.As<FilterBase>()->xWidth);
-	int x1 = math::Floor2Int(dimageX + filter.As<FilterBase>()->xWidth);
-	int y0 = math::Ceil2Int(dimageY - filter.As<FilterBase>()->yWidth);
-	int y1 = math::Floor2Int(dimageY + filter.As<FilterBase>()->yWidth);
-	x0 = max(x0, 0);
-	x1 = min(x1, 0 + int(w) - 1);
-	y0 = max(y0, 0);
-	y1 = min(y1, 0 + int(h) - 1);
+	int x0 = max(0, math::Ceil2Int(_x - filter.As<FilterBase>()->xWidth));
+	int x1 = min(w - 1, math::Floor2Int(_x + filter.As<FilterBase>()->xWidth));
+	int y0 = max(0, math::Ceil2Int(_y - filter.As<FilterBase>()->yWidth));
+	int y1 = min(h - 1, math::Floor2Int(_y + filter.As<FilterBase>()->yWidth));
 	if ((x1 - x0) < 0 || (y1 - y0) < 0)
 		return Spectrum(0.0f);
 	Spectrum acc(0.0f);
@@ -23,7 +17,7 @@ CUDA_FUNC_IN Spectrum evalFilter(const Filter& filter, PixelData* P, float splat
 	{
 		for (int x = x0; x <= x1; ++x)
 		{
-			float filterWt = filter.Evaluate(math::abs(x - dimageX), math::abs(y - dimageY));
+			float filterWt = filter.Evaluate(math::abs(x - _x), math::abs(y - _y));
 			acc += P[y * w + x].toSpectrum(splatScale) * filterWt;
 			accFilter += filterWt;
 		}
@@ -31,9 +25,9 @@ CUDA_FUNC_IN Spectrum evalFilter(const Filter& filter, PixelData* P, float splat
 	return acc / accFilter;
 }
 
-CUDA_GLOBAL void rtm_Copy(Image img, unsigned int w, unsigned int h, float splatScale, Filter filter)
+CUDA_GLOBAL void rtm_Copy(Image img, int w, int h, float splatScale, Filter filter)
 {
-	unsigned int x = threadIdx.x + blockDim.x * blockIdx.x, y = threadIdx.y + blockDim.y * blockIdx.y;
+	int x = threadIdx.x + blockDim.x * blockIdx.x, y = threadIdx.y + blockDim.y * blockIdx.y;
 	if (x < w && y < h)
 	{
 		Spectrum c = evalFilter(filter, &img.getPixelData(0, 0), splatScale, x, y, w, h);
@@ -44,7 +38,7 @@ CUDA_GLOBAL void rtm_Copy(Image img, unsigned int w, unsigned int h, float splat
 void CanonicalFilter::Apply(Image& img, int numPasses, float splatScale)
 {
 	int block = 32, xResolution = img.getWidth(), yResolution = img.getHeight();
-	rtm_Copy << <dim3(xResolution / block + 1, yResolution / block + 1), dim3(block, block) >> >(img, xResolution, yResolution, splatScale, m_filter);
+	rtm_Copy << <dim3(xResolution / block + 1, yResolution / block + 1), dim3(block, block) >> >(img, (int)xResolution, (int)yResolution, splatScale, m_filter);
 
 	ThrowCudaErrors(cudaThreadSynchronize());
 }
