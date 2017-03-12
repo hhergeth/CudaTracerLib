95bf24407e933bbc0b35d151b281a8d72c555e95
Hannes Hergeth <hannes.hergeth@rwth-aachen.de>
2017-03-12 16:07:47 +0100

    Correctly implemented stratified sampler

--- Kernel/Sampler.h
+++ Kernel/Sampler.h
@@ -35,6 +35,7 @@ public:
 
 	virtual void Compute(SequenceSamplerData& data)
 	{
+		_obj.NextPass();
 		auto seqLen = data.getSequenceLength();
 		float* sequence_1d = (float*)alloca(sizeof(float) * seqLen);
 		Vec2f* sequence_2d = (Vec2f*)alloca(sizeof(Vec2f) * seqLen);
@@ -67,6 +68,10 @@ public:
 		: rng(7539414)
 	{
 	}
+	void NextPass()
+	{
+
+	}
 	void Compute1D(float* sequence, unsigned int sequence_idx, unsigned int sequence_length)
 	{
 		for (unsigned int i = 0; i < sequence_length; i++)
@@ -82,18 +87,33 @@ public:
 class StratifiedSamplingSequenceGenerator
 {
 	const int n_strata;
+	CudaRNG rng;
+	unsigned int pass_idx;
 public:
 	StratifiedSamplingSequenceGenerator(int n_strata = 10)
-		: n_strata(n_strata)
+		: n_strata(n_strata), rng(7539414), pass_idx(0)
 	{
+	}
+	void NextPass()
+	{
+		pass_idx++;
 	}
 	void Compute1D(float* sequence, unsigned int sequence_idx, unsigned int sequence_length)
 	{
+		sequence[0] = math::frac((pass_idx + sequence_idx + rng.randomFloat()) / n_strata);
 
+		for(unsigned int i = 1u; i < sequence_length; i++)
+			sequence[i] = rng.randomFloat();
 	}
 	void Compute2D(Vec2f* sequence, unsigned int sequence_idx, unsigned int sequence_length)
 	{
+		auto j = (pass_idx + sequence_idx) % (n_strata * n_strata);
+		auto x = j % n_strata, y = j / n_strata;
+		auto sample = (Vec2f((float)x, (float)y) + rng.randomFloat2()) / n_strata;
+		sequence[0] = Vec2f(math::frac(sample.x), math::frac(sample.y));
 
+		for (unsigned int i = 1u; i < sequence_length; i++)
+			sequence[i] = rng.randomFloat2();
 	}
 };
 
