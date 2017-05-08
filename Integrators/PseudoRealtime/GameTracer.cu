a18683a8a566fae802726318c728798d1e9bc7d9
Hannes Hergeth <hannes.hergeth@rwth-aachen.de>
2017-05-09 01:33:01 +0200

    rewrote GameTracer to use Path Space Filtering

--- Integrators/PseudoRealtime/GameTracer.cu
+++ Integrators/PseudoRealtime/GameTracer.cu
@@ -6,202 +6,127 @@
 
 namespace CudaTracerLib {
 
-CUDA_DEVICE DeviceDepthImage g_DepthImageGT;
-
-#define INDIRECT_SCALE 1
-
-#define BLOCK_SIZE 16
-__global__ void primaryKernelBlocked(int width, int height, Image g_Image, bool depthImage,
-									 Spectrum* lastDirectImage, Spectrum* nextDirectImage, Spectrum* lastIndirectImage, Spectrum* nextIndirectImage,
-									 Sensor lastSensor, int nIteration)
+template<bool DIRECT> CUDA_FUNC_IN Spectrum PathTrace(NormalizedT<Ray>& r, Sampler& rnd, int maxPathLength, int rrStartDepth)
 {
-	int x = 2 * (blockIdx.x * blockDim.x + threadIdx.x), y = 2 * (blockIdx.y * blockDim.y + threadIdx.y);
-	auto rng = g_SamplerData(y * width + x);
-	if (x < width && y < height)
+	Spectrum cl = Spectrum(0.0f);   // accumulated color
+	Spectrum cf = Spectrum(1.0f);  // accumulated reflectance
+	int depth = 0;
+	bool specularBounce = false;
+	BSDFSamplingRecord bRec;
+	KernelAggregateVolume& V = g_SceneData.m_sVolume;
+	MediumSamplingRecord mRec;
+	TraceResult r2;
+	while (depth++ < maxPathLength)
 	{
-		BSDFSamplingRecord bRec;
-		NormalizedT<Ray> primaryRay;
-		TraceResult primaryRes;
+		r2 = traceRay(r);
+		float minT, maxT;
+		bool isInMedium = V.IntersectP(r, 0, r2.m_fDist, &minT, &maxT);
+		if (V.HasVolumes() && isInMedium && V.sampleDistance(r, 0, r2.m_fDist, rnd.randomFloat(), mRec))
+		{
+			cf *= mRec.sigmaS * mRec.transmittance / mRec.pdfSuccess;
 
-		Spectrum primary_Le(0.0f);//average of all 4 primary emittances
-		Spectrum primary_f[4];//this includes W_e and volume transmittance
-		Vec2f uv_sets[4];
-		int primary_rays_hit = 0;
-		for (int i = 0; i < 4; i++)
-		{
-			int x2 = x + i % 2, y2 = y + i / 2;
-			NormalizedT<Ray> r_i;
-			primary_f[i] = g_SceneData.m_Camera.sampleRay(r_i, Vec2f(x2, y2), rng.randomFloat2());
-			auto r2_i = traceRay(r_i);
-			primary_f[i] *= Transmittance(r_i, 0, r2_i.m_fDist);
-			if (r2_i.hasHit())
+			if (DIRECT)//direct sampling
 			{
-				primaryRay = r_i;
-				primaryRes = r2_i;
-				primary_rays_hit++;
-				primaryRes.getBsdfSample(primaryRay, bRec, ETransportMode::ERadiance);
-				uv_sets[i] = bRec.dg.uv[0];
-				primary_Le += primaryRes.Le(bRec.dg.P, bRec.dg.sys, -primaryRay.dir());
-			}
-			else primary_f[i] = g_SceneData.EvalEnvironment(primaryRay);
-			if (depthImage && x2 < width && y2 < height)
-				g_DepthImageGT.Store(x2, y2, primaryRes.m_fDist);
-		}
-		if (!primary_rays_hit)
-		{
-			nextDirectImage[y / 2 * width / 2 + x / 2] = nextIndirectImage[y / 2 * width / 2 + x / 2] = Spectrum(0.0f);
-			return;
-		}
-		primary_Le /= primary_rays_hit;
-
-		//do N_d next event estimation samples using the same input geometry
-		const int N_d = 8;
-		Spectrum Est_Ld(0.0f);
-		int direct_sampling_succes = 0;
-		Vec3f direct_sampling_point(0.0f);
-		for (int i = 0; i < N_d; i++)
-		{
-			float pdf;
-			Vec2f sample = rng.randomFloat2();
-			const Light* light = g_SceneData.sampleEmitter(pdf, sample);
-			if (!light) continue;
-			DirectSamplingRecord dRec(bRec.dg.P, bRec.dg.sys.n);
-			Spectrum value = light->sampleDirect(dRec, sample) / pdf;
-			if (!value.isZero() && !g_SceneData.Occluded(Ray(dRec.ref, dRec.d), 0, dRec.dist))
-			{
-				direct_sampling_succes++;
-				direct_sampling_point += dRec.p;
-				Est_Ld += value * Transmittance(Ray(dRec.ref, dRec.d), 0, dRec.dist);
-			}
-		}
-		Est_Ld /= direct_sampling_succes;
-		direct_sampling_point /= direct_sampling_succes;
-
-		//do N_i samples of indirect lighting = sample random point in the scene, compute primary f for each pixel and compute do next event estimation
-		const int N_i = 4;
-		Spectrum Est_Li[4];
-		for (int i = 0; i < 4; i++)
-			Est_Li[i] = Spectrum(0.0f);
-		for (int i = 0; i < N_i; i++)
-		{
-			primaryRes.getMat().bsdf.sample(bRec, rng.randomFloat2());
-			NormalizedT<Ray> indirect_ray = NormalizedT<Ray>(bRec.dg.P, bRec.getOutgoing());
-			auto indirect_res = traceRay(indirect_ray);
-			if (indirect_res.hasHit())
-			{
-				BSDFSamplingRecord indirect_bRec;
-				indirect_res.getBsdfSample(indirect_ray, indirect_bRec, ETransportMode::ERadiance);
-				float pdf;
-				Vec2f sample = rng.randomFloat2();
-				const Light* light = g_SceneData.sampleEmitter(pdf, sample);
-				if (!light) continue;
-				DirectSamplingRecord dRec(indirect_bRec.dg.P, indirect_bRec.dg.sys.n);
-				Spectrum value = light->sampleDirect(dRec, sample) / pdf;
-				if (!value.isZero() && !g_SceneData.Occluded(Ray(dRec.ref, dRec.d), 0, dRec.dist))
+				DirectSamplingRecord dRec(mRec.p, NormalizedT<Vec3f>(0.0f));
+				Spectrum value = g_SceneData.sampleAttenuatedEmitterDirect(dRec, rnd.randomFloat2());
+				if (!value.isZero())
 				{
-					indirect_bRec.wo = indirect_bRec.dg.sys.toLocal(dRec.d);
-					Spectrum indirect_f = indirect_res.getMat().bsdf.f(indirect_bRec);
-					for (int j = 0; j < 4; j++)
+					PhaseFunctionSamplingRecord pRec(-r.dir(), dRec.d);
+					float p = V.p(mRec.p, pRec);
+					if (p != 0 && !g_SceneData.Occluded(Ray(dRec.ref, dRec.d), 0, dRec.dist))
 					{
-						bRec.dg.uv[0] = uv_sets[j];
-						//not neccessary to set wo, still beeing set from sampling(different uv though...)
-						Est_Li[j] += primaryRes.getMat().bsdf.f(bRec) * indirect_f * value / float(N_i);
+						const float bsdfPdf = p;//phase functions are normalized
+						const float weight = MonteCarlo::PowerHeuristic(1, dRec.pdf, 1, bsdfPdf);
+						cl += cf * value * p * weight;
 					}
 				}
 			}
+
+			PhaseFunctionSamplingRecord pRec(-r.dir());
+			cf *= V.Sample(mRec.p, pRec, rnd.randomFloat2());
+			r.dir() = pRec.wo;
+			r.ori() = mRec.p;
 		}
-
-		//for noise reduction average the result with the last frames results
-		Spectrum avg_Est_Li(0.0f);
-		for (int i = 0; i < 4; i++)
-			avg_Est_Li += Est_Li[i] / 4.0f;
-
-		int Inew = (y / 2) * (width / 2) + (x / 2);
-		nextDirectImage[Inew] = Est_Ld;
-		nextIndirectImage[Inew] = avg_Est_Li;
-
-		if (nIteration > 2)
+		else if (r2.hasHit())
 		{
-			DirectSamplingRecord dRec(bRec.dg.P, NormalizedT<Vec3f>(0.0f));
-			lastSensor.sampleDirect(dRec, Vec2f(0, 0));
-			if (dRec.pdf)
-			{
-				const float p = 0.5f;
-				int oy = int(dRec.uv.y)/2, ox = int(dRec.uv.x)/2;
-#define LOAD(img, ax, ay) img[min(oy+ay,height/2-1) * (width / 2) +  min(ox+ax,width/2-1)]
-				if (direct_sampling_succes)
-				{
-					Spectrum lu = LOAD(lastDirectImage, 0, 0), ru = LOAD(lastDirectImage, 1, 0),
-							 ld = LOAD(lastDirectImage, 0, 1), rd = LOAD(lastDirectImage, 1, 1);
-					Spectrum lastDirect = math::bilerp2(lu, ru, ld, rd, dRec.uv - dRec.uv.floor());
-					if (lastDirect.max() != 0)
-						Est_Ld = math::lerp(lastDirect, Est_Ld, p);
-				}
-				Spectrum lu = LOAD(lastIndirectImage, 0, 0), ru = LOAD(lastIndirectImage, 1, 0),
-						 ld = LOAD(lastIndirectImage, 0, 1), rd = LOAD(lastIndirectImage, 1, 1);
-				Spectrum lastIndirect = math::bilerp2(lu, ru, ld, rd, dRec.uv - dRec.uv.floor());
-				if (lastIndirect.max() != 0)
-					for (int i = 0; i < 4; i++)
-						Est_Li[i] = math::lerp(lastIndirect, Est_Li[i], p);
-#undef LOAD
-			}
+			if (isInMedium)
+				cf *= mRec.transmittance / mRec.pdfFailure;
+			r2.getBsdfSample(r, bRec, ETransportMode::ERadiance);
+			if (!DIRECT || (depth == 1 || specularBounce))
+				cl += cf * r2.Le(bRec.dg.P, bRec.dg.sys, -r.dir());
+			Spectrum f = r2.getMat().bsdf.sample(bRec, rnd.randomFloat2());
+			if (DIRECT)
+				cl += cf * UniformSampleOneLight(bRec, r2.getMat(), rnd, true);
+			specularBounce = (bRec.sampledType & EDelta) != 0;
+			cf = cf * f;
+			r = NormalizedT<Ray>(bRec.dg.P, bRec.getOutgoing());
 		}
-
-		for (int i = 0; i < 4; i++)
+		if (depth > rrStartDepth)
 		{
-			Spectrum direct(0.0f);
-			if (direct_sampling_succes)
+			if (rnd.randomFloat() >= cf.max())
+				break;
+			cf /= cf.max();
+		}
+	}
+	if (!r2.hasHit())
+		cl += cf * g_SceneData.EvalEnvironment(r);
+	return cl;
+}
+
+CUDA_DEVICE CudaStaticWrapper<PathSpaceFilteringBuffer> g_Buffer;
+#define PIXEL_SPACING 16
+
+CUDA_GLOBAL void createHitPointKernel(unsigned int w, unsigned int h)
+{
+	unsigned int x = PIXEL_SPACING * (threadIdx.x + blockDim.x * blockIdx.x), y = PIXEL_SPACING * (threadIdx.y + blockDim.y * blockIdx.y);
+
+	if (x < w && y < h)
+	{
+		auto rng = g_SamplerData(y * w + x);
+		NormalizedT<Ray> r;
+		g_SceneData.sampleSensorRay(r, Vec2f(x, y), rng.randomFloat2());
+
+		auto res = traceRay(r);
+		if (res.hasHit())
+		{
+			BSDFSamplingRecord bRec;
+			res.getBsdfSample(r, bRec, ERadiance);
+			if (res.getMat().bsdf.hasComponent(EDiffuse))
 			{
-				bRec.dg.uv[0] = uv_sets[i];
-				bRec.wo = normalize(bRec.dg.sys.toLocal(direct_sampling_point - bRec.dg.P));
-				direct = Est_Ld * primaryRes.getMat().bsdf.f(bRec);
+				res.getMat().bsdf.sample(bRec, rng.randomFloat2());
+				auto Li = PathTrace<true>(NormalizedT<Ray>(bRec.dg.P, bRec.getOutgoing()), rng, 6, 4);
+				g_Buffer->add_sample(bRec.dg, bRec.wo, Li);
 			}
-			Spectrum L = primary_f[i] * (primary_Le + direct) + Est_Li[i] * INDIRECT_SCALE;
-			int x2 = x + i % 2, y2 = y + i / 2;
-			if (x2 < width && y2 < height)
-				g_Image.AddSample(x2, y2, L);
 		}
 	}
 }
 
 void GameTracer::DoRender(Image* I)
 {
-	if (hasDepthBuffer())
-		CopyToSymbol(g_DepthImageGT, getDeviceDepthBuffer());
-	primaryKernelBlocked << <dim3(w / (2 * BLOCK_SIZE) + 1, h / (2 * BLOCK_SIZE) + 1, 1), dim3(BLOCK_SIZE, BLOCK_SIZE, 1) >> >(w, h, *I, hasDepthBuffer(),
-		m_pDeviceLastDirectImage1, m_pDeviceLastDirectImage2, m_pDeviceLastIndirectImage1, m_pDeviceLastIndirectImage2, lastSensor, iterations++);
-	lastSensor = g_SceneData.m_Camera;
-	swapk(m_pDeviceLastDirectImage1, m_pDeviceLastDirectImage2);
-	swapk(m_pDeviceLastIndirectImage1, m_pDeviceLastIndirectImage2);
+	static bool init = false;
+	if (!init)
+	{
+		init = true;
+		buf.PrepareForRendering(*I, m_pScene);
+	}
+	buf.StartFrame(I->getWidth(), I->getHeight());
+	CopyToSymbol(g_Buffer, buf);
+	int p0 = 16, p1 = p0 * PIXEL_SPACING;
+	createHitPointKernel << <dim3(I->getWidth() / p1 + 1, I->getHeight() / p1 + 1, 1), dim3(p0, p0, 1) >> >(I->getWidth(), I->getHeight());
+	CopyFromSymbol(buf, g_Buffer);
+	buf.ComputePixelValues(*I, m_pScene, hasDepthBuffer() ? &this->getDeviceDepthBuffer() : 0);
 }
 
 void GameTracer::DebugInternal(Image* I, const Vec2i& pixel)
 {
-	auto rng = g_SamplerData(pixel.y * I->getWidth() + pixel.x);
-	NormalizedT<Ray> r, rX, rY;
-	g_SceneData.sampleSensorRay(r, rX, rY, Vec2f((float)pixel.x, (float)pixel.y), rng.randomFloat2());
+
 }
 
 void GameTracer::Resize(unsigned int w, unsigned int h)
 {
-	if (m_pDeviceLastDirectImage1)
-	{
-		CUDA_FREE(m_pDeviceLastDirectImage1);
-		CUDA_FREE(m_pDeviceLastDirectImage2);
-		CUDA_FREE(m_pDeviceLastIndirectImage1);
-		CUDA_FREE(m_pDeviceLastIndirectImage2);
-	}
-	unsigned int _w = w / 2, _h = h / 2;
-	CUDA_MALLOC(&m_pDeviceLastDirectImage1, _w * _h * sizeof(Spectrum));
-	CUDA_MALLOC(&m_pDeviceLastDirectImage2, _w * _h * sizeof(Spectrum));
-	CUDA_MALLOC(&m_pDeviceLastIndirectImage1, _w * _h * sizeof(Spectrum));
-	CUDA_MALLOC(&m_pDeviceLastIndirectImage2, _w * _h * sizeof(Spectrum));
-	cudaMemset(m_pDeviceLastDirectImage1, 0, _w * _h * sizeof(Spectrum));
-	cudaMemset(m_pDeviceLastDirectImage2, 0, _w * _h * sizeof(Spectrum));
-	cudaMemset(m_pDeviceLastIndirectImage1, 0, _w * _h * sizeof(Spectrum));
-	cudaMemset(m_pDeviceLastIndirectImage2, 0, _w * _h * sizeof(Spectrum));
-	Platform::SetMemory(&lastSensor, sizeof(lastSensor));
-	iterations = 0;
+	buf.ResizeHitPointBuffer(w / PIXEL_SPACING * h / PIXEL_SPACING);
+	buf.getParameterCollection().setValue(PathSpaceFilteringBuffer::KEY_PixelFootprintScale(), (float)PIXEL_SPACING / 2);
+	buf.getParameterCollection().setValue(PathSpaceFilteringBuffer::KEY_GlobalRadiusScale(), (float)PIXEL_SPACING / 2);
 	Tracer<false>::Resize(w, h);
 }
 
--- Integrators/PseudoRealtime/GameTracer.h
+++ Integrators/PseudoRealtime/GameTracer.h
@@ -1,21 +1,18 @@
 #pragma once
 
 #include <Kernel/Tracer.h>
+#include <Kernel/PathSpaceFilteringBuffer.h>
 
 namespace CudaTracerLib {
 
 class GameTracer : public Tracer<false>, public IDepthTracer
 {
-	Spectrum* m_pDeviceLastDirectImage1, *m_pDeviceLastDirectImage2;
-	Spectrum* m_pDeviceLastIndirectImage1, *m_pDeviceLastIndirectImage2;
-	Sensor lastSensor;
-	int iterations;
+	PathSpaceFilteringBuffer buf;
 public:
 	GameTracer()
-		: m_pDeviceLastDirectImage1(0), m_pDeviceLastDirectImage2(0),
-		  m_pDeviceLastIndirectImage1(0), m_pDeviceLastIndirectImage2(0), iterations(0)
+		: buf(1)
 	{
-
+		m_sParameters.addChildParameterCollection("PathSpaceFilterBuffer", &buf.getParameterCollection());
 	}
 	CTL_EXPORT virtual void Resize(unsigned int _w, unsigned int _h);
 protected:
