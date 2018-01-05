480bf10ddbd37aaba2f5b2676734e62940c1500a
Hannes Hergeth <hannes.hergeth@rwth-aachen.de>
2018-01-05 19:52:05 +0100

    added intelligent texture path query

--- Engine/DynamicScene.cpp
+++ Engine/DynamicScene.cpp
@@ -354,10 +354,13 @@ void DynamicScene::DeleteNode(StreamReference<Node> ref)
 
 BufferReference<MIPMap, KernelMIPMap> DynamicScene::LoadTexture(const std::string& file, bool a_MipMap)
 {
-	path rawFilePath(m_pFileManager->getTexturePath(file));
+	path rawFilePath = file;
+	if (!exists(rawFilePath) || is_directory(rawFilePath))
+		rawFilePath = m_pFileManager->getTexturePath(file);
+
 	if (!exists(rawFilePath) || is_directory(rawFilePath))
 	{
-		std::cout << "Texture : " << file << " was not found\n";
+		std::cout << "Texture : " << file << "mapped to : " << rawFilePath << " was not found\n";
 		return LoadTexture("404.jpg", a_MipMap);
 	}
 	bool load;
